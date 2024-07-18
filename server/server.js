7b042536000f7f512984ef2c22f4ffd87b7cdae0
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-07-18 23:12:00 +0530

    integrate sentry on node.js backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 // Load environment variables
 require("dotenv").config();
+require("./configs/sentry-instrument.js");
 
+const Sentry = require("@sentry/node");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -10,7 +12,7 @@ const interviewRoute = require("./routes/interview.js");
 const impromptuSpeakingRoute = require("./routes/impromptu-speaking.js");
 
 // Log requests in 'dev' format
-app.use(morgan("common"));
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   if (req.path.endsWith(".json")) {
@@ -37,10 +39,25 @@ app.get("/", (req, res, next) => {
 app.use("/interview", interviewRoute);
 app.use("/impromptu-speaking", impromptuSpeakingRoute);
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry testing error!");
+});
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 const IP = "0.0.0.0";
