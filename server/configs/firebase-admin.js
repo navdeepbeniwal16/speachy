81b727e229dc0746a9f4382ad31c6c1bcad67bc6
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-08-20 15:00:59 +1000

    move service account config variables to env variables

--- server/configs/firebase-admin.js
+++ server/configs/firebase-admin.js
@@ -1,6 +1,22 @@
+// Load environment variables
+require("dotenv").config();
+
 const { initializeApp, cert } = require("firebase-admin/app");
 
-const serviceAccount = require("../firebase-admin-security.json");
+// const serviceAccount = require("../firebase-admin-security.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FB_DEV_PROJECT_ID,
+  private_key_id: process.env.FB_DEV_PRIVATE_KEY_ID,
+  private_key: process.env.FB_DEV_PRIVATE_KEY,
+  client_email: process.env.FB_DEV_CLIENT_EMAIL,
+  client_id: process.env.FB_DEV_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FB_DEV_CLIENT_X509_CERT_URL,
+  universe_domain: "googleapis.com",
+};
 
 const firebaseAdmin = initializeApp({
   credential: cert(serviceAccount),
