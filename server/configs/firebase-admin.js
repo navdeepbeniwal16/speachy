77b59836cf7b0d0fc35bae535a8b29139f63fd77
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-08-20 21:10:17 +1000

    pas firebase-admin config as env variable in base64

--- server/configs/firebase-admin.js
+++ server/configs/firebase-admin.js
@@ -3,20 +3,13 @@ require("dotenv").config();
 
 const { initializeApp, cert } = require("firebase-admin/app");
 
-// const serviceAccount = require("../firebase-admin-security.json");
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FB_DEV_PROJECT_ID,
-  private_key_id: process.env.FB_DEV_PRIVATE_KEY_ID,
-  private_key: process.env.FB_DEV_PRIVATE_KEY,
-  client_email: process.env.FB_DEV_CLIENT_EMAIL,
-  client_id: process.env.FB_DEV_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.FB_DEV_CLIENT_X509_CERT_URL,
-  universe_domain: "googleapis.com",
-};
+const base64EncodedServiceAccount =
+  process.env.FB_DEV_BASE64_ENCODED_SERVICE_ACCOUNT;
+const decodedServiceAccount = Buffer.from(
+  base64EncodedServiceAccount,
+  "base64"
+).toString("utf-8");
+const serviceAccount = JSON.parse(decodedServiceAccount);
 
 const firebaseAdmin = initializeApp({
   credential: cert(serviceAccount),
