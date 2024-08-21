caafe78131dbfd56fdfae85d7a0da99549ebf8e6
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-08-21 13:52:10 +1000

    show user and env info

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -13,9 +13,11 @@ import {
   MenuItem,
   Drawer,
   Chip,
+  Badge,
+  Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import Avatar from "@mui/material/Avatar";
+import SettingsIcon from "@mui/icons-material/Settings";
 import PersonIcon from "@mui/icons-material/Person";
 import PaymentsIcon from "@mui/icons-material/Payments";
 import Logout from "@mui/icons-material/Logout";
@@ -30,19 +32,6 @@ const logoStyle = {
   cursor: "pointer",
   marginRight: "16px",
   marginTop: "4px",
-};
-
-const getDisplayNameInitials = (displayName) => {
-  if (!displayName || displayName.length === 0) {
-    return "N/A";
-  } else {
-    const nameSplitted = displayName.split(" ");
-    if (nameSplitted.length > 1) {
-      return nameSplitted[0][0] + nameSplitted[1][0];
-    } else {
-      return nameSplitted[0][0];
-    }
-  }
 };
 
 // Component to display drop down menu including options like 'Profile', 'Payments' & 'Signout' button
@@ -73,16 +62,14 @@ const AccountMenu = ({
             aria-haspopup="true"
             aria-expanded={open ? "true" : undefined}
           >
-            <Avatar
+            <SettingsIcon
               sx={{
-                bgcolor: deepOrange[400],
+                color: deepOrange[400],
                 width: 32,
                 height: 32,
                 fontSize: "12px",
               }}
-            >
-              {user && getDisplayNameInitials(user.displayName)}
-            </Avatar>
+            ></SettingsIcon>
           </IconButton>
         </Tooltip>
       </Box>
@@ -121,6 +108,12 @@ const AccountMenu = ({
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
+        <MenuItem>
+          <Typography variant="subtitle2" color="text.secondary">
+            {user.email}
+          </Typography>
+        </MenuItem>
+
         <MenuItem onClick={handleProfileClick} sx={{ gap: 1 }}>
           <PersonIcon style={{ color: "gray" }} /> Profile{" "}
           {isPaidCustomer && (
@@ -192,6 +185,19 @@ const NavigationBar = () => {
     }
   };
 
+  const getEnvironmentLabel = () => {
+    const env = process.env.REACT_APP_ENV;
+    if (env === "local") {
+      return "Local";
+    } else if (env === "development") {
+      return "Development";
+    } else if (env === "production") {
+      return "Early Access";
+    }
+
+    return "Unknown";
+  };
+
   return (
     <div>
       <AppBar
@@ -224,11 +230,20 @@ const NavigationBar = () => {
               }}
             >
               <Link to="/">
-                <img
-                  src="/assets/Speachy_Logo_SVG.svg"
-                  style={logoStyle}
-                  alt="logo of speachy"
-                />
+                <Badge
+                  color="warning"
+                  badgeContent={getEnvironmentLabel()}
+                  anchorOrigin={{
+                    vertical: "center",
+                    horizontal: "center",
+                  }}
+                >
+                  <img
+                    src="/assets/Speachy_Logo_SVG.svg"
+                    style={logoStyle}
+                    alt="logo of speachy"
+                  />
+                </Badge>
               </Link>
             </Box>
             {user ? (
@@ -239,6 +254,10 @@ const NavigationBar = () => {
                   alignItems: "center",
                 }}
               >
+                <Typography variant="subtitle1" color="black">
+                  {auth.currentUser.displayName}
+                </Typography>
+
                 <AccountMenu
                   user={user}
                   handleSignoutClick={signOut}
