1a66583238cda90e437f704c5c8e7b1ec6adc540
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-08-26 16:29:54 +1000

    Make home page responsive

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -79,12 +79,9 @@ const HomePage = () => {
   };
 
   return (
-    <Container
-      maxWidth="lg"
-      sx={{ mt: 4, pt: 4, pb: 8, px: 8, fontFamily: "Roboto" }}
-    >
+    <Container maxWidth="lg" sx={{ mt: 4, pt: 4, pb: 4, fontFamily: "Roboto" }}>
       <Box sx={{ textAlign: "center", mb: 8 }}>
-        <Typography variant="h2" gutterBottom>
+        <Typography variant="h3" gutterBottom>
           Welcome{" "}
           <span style={{ color: "darkorange", fontWeight: "bold" }}>
             {auth.currentUser.displayName.split(" ")[0]}
@@ -97,7 +94,7 @@ const HomePage = () => {
         </Typography>
       </Box>
 
-      <Box id="interview-section" sx={{ mb: 8 }}>
+      <Box sx={{ mb: 8 }}>
         <Typography
           variant="h4"
           sx={{ textAlign: "center", mb: 4 }}
@@ -106,9 +103,9 @@ const HomePage = () => {
           What are we working on today?
         </Typography>
 
-        <Grid id="catalogue" container spacing={4}>
-          <Grid item xs={12} sm={6} md={1}></Grid>
-          <Grid item xs={12} sm={6} md={5}>
+        <Grid id="catalogue" container spacing={2}>
+          <Grid item xs={0} sm={0} md={1}></Grid>
+          <Grid item xs={12} sm={12} md={5}>
             <Card
               variant="elevation"
               sx={{
@@ -141,6 +138,7 @@ const HomePage = () => {
                   <Typography
                     variant="body2"
                     color="text.secondary"
+                    sx={{ textAlign: "center" }}
                     gutterBottom
                   >
                     Boost your impromptu speaking with practice and feedback!
@@ -158,7 +156,7 @@ const HomePage = () => {
               </CardActionArea>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={6} md={5}>
+          <Grid item xs={12} sm={12} md={5}>
             <Card
               variant="elevation"
               sx={{
@@ -188,7 +186,11 @@ const HomePage = () => {
                   >
                     👔 Job Interview Preparation
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{ textAlign: "center" }}
+                  >
                     Ace your interviews with practice questions and response
                     analysis!
                   </Typography>
@@ -205,7 +207,7 @@ const HomePage = () => {
               </CardActionArea>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={6} md={1}></Grid>
+          <Grid item xs={0} sm={0} md={1}></Grid>
         </Grid>
       </Box>
 
