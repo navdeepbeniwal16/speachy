f20dfda87ee972b099bb892c14c3c9fb66ebaa5b
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-11-13 15:51:47 +1100

    configure client to send auth token to server

--- client/src/services/backendAPIClient.js
+++ client/src/services/backendAPIClient.js
@@ -1,17 +1,33 @@
 import axios from "axios";
+import { getAuth } from "firebase/auth";
 
 const backendApiClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  // baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// Interceptor to add the token to each request
+backendApiClient.interceptors.request.use(
+  async (config) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      const idToken = await user.getIdToken(); // Wait for the token
+      config.headers.Authorization = `Bearer ${idToken}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Interceptor for global error handling
 backendApiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    // Global error handling
     console.error("REST API Client Network Error:", error.response);
     return Promise.reject(error);
   }
